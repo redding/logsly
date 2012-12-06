60d3bfc58f54acce11621f3b883e6fcbf1c84ba7
Kelly <kelly@kellyredding.com>
2012-12-06 15:45:29 -0600

    have stdout outputs build an appropriate logging appender

--- lib/logsly/stdout_output.rb
+++ lib/logsly/stdout_output.rb
@@ -1,7 +1,11 @@
+require 'logging'
 require 'logsly/base_output'
 
 module Logsly
   class StdoutOutput < BaseOutput
-
+    def to_appender
+      @appender = Logging.appenders.stdout(:layout => self.to_layout) if @appender.nil?
+      @appender
+    end
   end
 end
--- test/unit/stdout_output_tests.rb
+++ test/unit/stdout_output_tests.rb
@@ -1,4 +1,6 @@
 require 'assert'
+require 'ostruct'
+require 'logging'
 require 'logsly/stdout_output'
 
 class Logsly::StdoutOutput
@@ -6,7 +8,18 @@ class Logsly::StdoutOutput
   class BaseTests < Assert::Context
     desc "the StdoutOutput handler"
     setup do
-      @out = Logsly::StdoutOutput.new {}
+      @logger = OpenStruct.new
+      @logger.debug_level = :white
+      @logger.pattern = '%d : %m\n'
+
+      Logsly.colors('a_color_scheme') do |logger|
+        debug logger.debug_level
+      end
+
+      @out = Logsly::StdoutOutput.new do |logger|
+        pattern logger.pattern
+        colors  'a_color_scheme'
+      end
     end
     subject { @out }
 
@@ -14,8 +27,14 @@ class Logsly::StdoutOutput
       assert_kind_of Logsly::BaseOutput, subject
     end
 
-    should "build a Logging stdout appender"
+    should "build a Logging stdout appender, passing args to the builds" do
+      subject.run_build @logger
+
+      assert_kind_of Logging::Appenders::Stdout, subject.to_appender
+      assert_kind_of Logging::Layouts::Pattern, subject.to_appender.layout
+      assert_equal   '%d : %m\n', subject.to_appender.layout.pattern
+      assert_kind_of Logging::ColorScheme, subject.to_appender.layout.color_scheme
+    end
   end
 
 end
-
